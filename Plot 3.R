7e8dba753ef8e95e0c8da012274c1cfd7732e879
Lauren <mmacbrewi1@yahoo.com>
2016-06-13 03:13:43 -0400

    Update Plot 3.R

--- Plot 3.R
+++ Plot 3.R
@@ -4,9 +4,9 @@ plot3 <- function() {
 
 png("plot3.png", width=400, height=400)
 
-  plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
-  lines(df$timestamp,df$Sub_metering_2,col="red")
-  lines(df$timestamp,df$Sub_metering_3,col="blue")
+  plot(data$timestamp,data$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+  lines(data$timestamp,data$Sub_metering_2,col="red")
+  lines(data$timestamp,data$Sub_metering_3,col="blue")
   legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
   dev.copy(png, file="plot3.png", width=480, height=480)
   dev.off()
