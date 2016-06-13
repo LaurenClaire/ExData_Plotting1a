d880d193247fa9e42693ef6a83088c5eba42cd86
Lauren <mmacbrewi1@yahoo.com>
2016-06-13 03:10:46 -0400

    Update Plot 2.R

--- Plot 2.R
+++ Plot 2.R
@@ -1,8 +1,8 @@
 #Plot 2
 
 plot2 <- function() {
-  plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+  plot(data$timestamp,data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
   dev.copy(png, file="plot2.png", width=480, height=480)
   dev.off()
-  cat
+  
 }
